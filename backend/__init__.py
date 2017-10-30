f751fa473e7f9578789d9194b89b18c2a5b46171
=Rami Ossmane <=rami-ossma@live.com>
2017-10-30 14:14:55 -0400

    split function

--- backend/__init__.py
+++ backend/__init__.py
@@ -1,4 +1,5 @@
 #COMMENT HELLO
 def process_user_query(query_string):
-    result = {"one": 1, "two": 2, "three":3 ,"query":query_string}
-    return result
+    return query_string.split();
+
+print(process_user_query("Hello friend"))
